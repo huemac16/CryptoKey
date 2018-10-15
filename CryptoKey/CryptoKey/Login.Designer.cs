27843664d5de86b3d1d9daee45c3b046cd24a1de
huemac16 <huemac16@htl-kaindorf.at>
2018-10-15 20:21:09 +0200

    creatred gui

---
+++ CryptoKey/CryptoKey/Login.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace CryptoKey
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbUsername = new System.Windows.Forms.Label();
+            this.tfUsername = new System.Windows.Forms.TextBox();
+            this.lbPassword = new System.Windows.Forms.Label();
+            this.tfPassword = new System.Windows.Forms.TextBox();
+            this.btLogin = new System.Windows.Forms.Button();
+            this.btRegister = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbUsername
+            // 
+            this.lbUsername.Location = new System.Drawing.Point(92, 50);
+            this.lbUsername.Name = "lbUsername";
+            this.lbUsername.Size = new System.Drawing.Size(86, 17);
+            this.lbUsername.TabIndex = 0;
+            this.lbUsername.Text = "Benutzername";
+            // 
+            // tfUsername
+            // 
+            this.tfUsername.Location = new System.Drawing.Point(95, 71);
+            this.tfUsername.Name = "tfUsername";
+            this.tfUsername.Size = new System.Drawing.Size(267, 20);
+            this.tfUsername.TabIndex = 1;
+            // 
+            // lbPassword
+            // 
+            this.lbPassword.Location = new System.Drawing.Point(92, 129);
+            this.lbPassword.Name = "lbPassword";
+            this.lbPassword.Size = new System.Drawing.Size(86, 17);
+            this.lbPassword.TabIndex = 2;
+            this.lbPassword.Text = "passwort";
+            // 
+            // tfPassword
+            // 
+            this.tfPassword.Location = new System.Drawing.Point(95, 161);
+            this.tfPassword.Name = "tfPassword";
+            this.tfPassword.Size = new System.Drawing.Size(267, 20);
+            this.tfPassword.TabIndex = 3;
+            // 
+            // btLogin
+            // 
+            this.btLogin.Location = new System.Drawing.Point(95, 217);
+            this.btLogin.Name = "btLogin";
+            this.btLogin.Size = new System.Drawing.Size(75, 23);
+            this.btLogin.TabIndex = 4;
+            this.btLogin.Text = "Anmelden";
+            this.btLogin.UseVisualStyleBackColor = true;
+            // 
+            // btRegister
+            // 
+            this.btRegister.Location = new System.Drawing.Point(286, 216);
+            this.btRegister.Name = "btRegister";
+            this.btRegister.Size = new System.Drawing.Size(75, 23);
+            this.btRegister.TabIndex = 5;
+            this.btRegister.Text = "Registrieren";
+            this.btRegister.UseVisualStyleBackColor = true;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(451, 284);
+            this.Controls.Add(this.btRegister);
+            this.Controls.Add(this.btLogin);
+            this.Controls.Add(this.tfPassword);
+            this.Controls.Add(this.lbPassword);
+            this.Controls.Add(this.tfUsername);
+            this.Controls.Add(this.lbUsername);
+            this.Name = "Login";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbUsername;
+        private System.Windows.Forms.TextBox tfUsername;
+        private System.Windows.Forms.Label lbPassword;
+        private System.Windows.Forms.TextBox tfPassword;
+        private System.Windows.Forms.Button btLogin;
+        private System.Windows.Forms.Button btRegister;
+    }
+}
+
