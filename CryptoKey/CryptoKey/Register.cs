eb7bfbe60423f6a34839da5f982fb2470f4f731d
huemac16 <huemac16@htl-kaindorf.at>
2018-10-15 21:09:48 +0200

    update

---
+++ CryptoKey/CryptoKey/Register.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CryptoKey
+{
+    public partial class Register : Form
+    {
+        private Login ancestor = new Login();
+        public Register(Login ancestor)
+        {
+            InitializeComponent();
+            this.ancestor = ancestor;
+        }
+
+        private void btCancel_Click(object sender, EventArgs e)
+        {
+            ancestor.setVisible(true);
+            this.Dispose();
+        }
+
+        public void setVisible(bool b)
+        {
+            this.SetVisibleCore(b);
+        }
+    }
+}
