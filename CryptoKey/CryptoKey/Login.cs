dfefd1ed73532302a6e842c4ecacf00ffde496de
mroth19847 <rotmac16@htl-kaindorf.at>
2018-10-15 20:41:56 +0200

    login

--- CryptoKey/CryptoKey/Login.cs
+++ CryptoKey/CryptoKey/Login.cs
@@ -12,9 +12,21 @@ namespace CryptoKey
 {
     public partial class Login : Form
     {
+        private CryptoKeyBL bl = new CryptoKeyBL();
         public Login()
         {
             InitializeComponent();
+        }
+
+        private void btLogin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                bl.LoginPerUsername(tfUsername.Text, tfPassword.Text);
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
