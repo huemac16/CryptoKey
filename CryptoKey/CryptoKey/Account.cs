74ebb2474302932465350e5d25030af61e1fcb5e
mroth19847 <rotmac16@htl-kaindorf.at>
2018-10-15 22:12:30 +0200

    update

---
+++ CryptoKey/CryptoKey/Account.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoKey
+{
+    public class Account
+    {
+        public string Title { get; set; }
+        public string Email { get; set; }
+        public string Onlineuser { get; set; }
+        public string Password { get; set; }
+        public string Url { get; set; }
+        public char Priority { get; set; }
+        public bool marked { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("{0} - {1}",Title.ToUpper(),Email);
+        }
+    }
+}
