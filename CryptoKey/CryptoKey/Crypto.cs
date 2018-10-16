f272895d827342598a17c8da194f13a6c9bd0f1d
huemac16 <huemac16@htl-kaindorf.at>
2018-10-16 09:04:14 +0200

    created crypto class

---
+++ CryptoKey/CryptoKey/Crypto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoKey
+{
+    public class Crypto
+    {
+        public String encrypt(String s)
+        {
+
+            return "";
+        }
+
+        public String decrypt(String s)
+        {
+
+            return "";
+        }
+    }
+}
