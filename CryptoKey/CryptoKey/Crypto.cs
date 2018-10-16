c5213f269dd1bc3750b4b2bc0e99fabfb9f6f8d0
huemac16 <huemac16@htl-kaindorf.at>
2018-10-16 09:44:27 +0200

    update

--- CryptoKey/CryptoKey/Crypto.cs
+++ CryptoKey/CryptoKey/Crypto.cs
@@ -8,8 +8,17 @@ namespace CryptoKey
 {
     public class Crypto
     {
+        private Random rand = new Random();
         public String encrypt(String s)
         {
+            int tmp = 0;
+            for(int i = 0; i < s.Length; i++)
+            {
+                tmp = i;
+                s.Insert(tmp + 1, "" + rand.Next(0, 9));
+                tmp = i++;
+                
+            }
 
             return "";
         }
