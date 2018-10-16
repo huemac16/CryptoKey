0a61736ada86885983cdcd4c69a049ff6589a52e
mroth19847 <rotmac16@htl-kaindorf.at>
2018-10-16 09:44:27 +0200

    update

--- CryptoKey/CryptoKey/Login.Designer.cs
+++ CryptoKey/CryptoKey/Login.Designer.cs
@@ -34,6 +34,9 @@
             this.tfPassword = new System.Windows.Forms.TextBox();
             this.btRegister = new System.Windows.Forms.Button();
             this.btLogin = new System.Windows.Forms.Button();
+            this.pnTest1 = new System.Windows.Forms.Panel();
+            this.pnTest2 = new System.Windows.Forms.Panel();
+            this.lbTest1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lbUsername
@@ -48,7 +51,7 @@
             // tfUsername
             // 
             this.tfUsername.Location = new System.Drawing.Point(190, 137);
-            this.tfUsername.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.tfUsername.Margin = new System.Windows.Forms.Padding(6);
             this.tfUsername.Name = "tfUsername";
             this.tfUsername.Size = new System.Drawing.Size(530, 31);
             this.tfUsername.TabIndex = 1;
@@ -65,7 +68,7 @@
             // tfPassword
             // 
             this.tfPassword.Location = new System.Drawing.Point(190, 310);
-            this.tfPassword.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.tfPassword.Margin = new System.Windows.Forms.Padding(6);
             this.tfPassword.Name = "tfPassword";
             this.tfPassword.Size = new System.Drawing.Size(530, 31);
             this.tfPassword.TabIndex = 3;
@@ -74,7 +77,7 @@
             // btRegister
             // 
             this.btRegister.Location = new System.Drawing.Point(572, 415);
-            this.btRegister.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btRegister.Margin = new System.Windows.Forms.Padding(6);
             this.btRegister.Name = "btRegister";
             this.btRegister.Size = new System.Drawing.Size(150, 44);
             this.btRegister.TabIndex = 5;
@@ -92,18 +95,46 @@
             this.btLogin.UseVisualStyleBackColor = true;
             this.btLogin.Click += new System.EventHandler(this.btLogin_Click);
             // 
+            // pnTest1
+            // 
+            this.pnTest1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pnTest1.Location = new System.Drawing.Point(899, 137);
+            this.pnTest1.Name = "pnTest1";
+            this.pnTest1.Size = new System.Drawing.Size(200, 100);
+            this.pnTest1.TabIndex = 7;
+            // 
+            // pnTest2
+            // 
+            this.pnTest2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pnTest2.Location = new System.Drawing.Point(899, 281);
+            this.pnTest2.Name = "pnTest2";
+            this.pnTest2.Size = new System.Drawing.Size(200, 100);
+            this.pnTest2.TabIndex = 8;
+            // 
+            // lbTest1
+            // 
+            this.lbTest1.AutoSize = true;
+            this.lbTest1.Location = new System.Drawing.Point(894, 434);
+            this.lbTest1.Name = "lbTest1";
+            this.lbTest1.Size = new System.Drawing.Size(48, 25);
+            this.lbTest1.TabIndex = 9;
+            this.lbTest1.Text = "Lan";
+            // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(902, 546);
+            this.ClientSize = new System.Drawing.Size(1231, 546);
+            this.Controls.Add(this.lbTest1);
+            this.Controls.Add(this.pnTest2);
+            this.Controls.Add(this.pnTest1);
             this.Controls.Add(this.btLogin);
             this.Controls.Add(this.btRegister);
             this.Controls.Add(this.tfPassword);
             this.Controls.Add(this.lbPassword);
             this.Controls.Add(this.tfUsername);
             this.Controls.Add(this.lbUsername);
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "Login";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -119,6 +150,9 @@
         private System.Windows.Forms.TextBox tfPassword;
         private System.Windows.Forms.Button btRegister;
         private System.Windows.Forms.Button btLogin;
+        private System.Windows.Forms.Panel pnTest1;
+        private System.Windows.Forms.Panel pnTest2;
+        private System.Windows.Forms.Label lbTest1;
     }
 }
 
