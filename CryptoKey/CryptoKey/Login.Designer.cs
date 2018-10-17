111437ba7fa6956676501f982e7f94d789522c7e
mroth19847 <rotmac16@htl-kaindorf.at>
2018-10-17 14:35:09 +0200

    update

--- CryptoKey/CryptoKey/Login.Designer.cs
+++ CryptoKey/CryptoKey/Login.Designer.cs
@@ -42,11 +42,12 @@
             // 
             // lbUsername
             // 
-            this.lbUsername.Font = new System.Drawing.Font("Roboto Th", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbUsername.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
-            this.lbUsername.Location = new System.Drawing.Point(368, 109);
+            this.lbUsername.Location = new System.Drawing.Point(736, 210);
+            this.lbUsername.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lbUsername.Name = "lbUsername";
-            this.lbUsername.Size = new System.Drawing.Size(86, 25);
+            this.lbUsername.Size = new System.Drawing.Size(172, 48);
             this.lbUsername.TabIndex = 0;
             this.lbUsername.Text = "Benutzername";
             // 
@@ -57,18 +58,20 @@
             this.tfUsername.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.tfUsername.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tfUsername.ForeColor = System.Drawing.Color.Black;
-            this.tfUsername.Location = new System.Drawing.Point(372, 137);
+            this.tfUsername.Location = new System.Drawing.Point(744, 263);
+            this.tfUsername.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.tfUsername.Name = "tfUsername";
-            this.tfUsername.Size = new System.Drawing.Size(267, 33);
+            this.tfUsername.Size = new System.Drawing.Size(532, 59);
             this.tfUsername.TabIndex = 1;
             // 
             // lbPassword
             // 
-            this.lbPassword.Font = new System.Drawing.Font("Roboto Th", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbPassword.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
-            this.lbPassword.Location = new System.Drawing.Point(368, 202);
+            this.lbPassword.Location = new System.Drawing.Point(736, 388);
+            this.lbPassword.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lbPassword.Name = "lbPassword";
-            this.lbPassword.Size = new System.Drawing.Size(86, 26);
+            this.lbPassword.Size = new System.Drawing.Size(196, 50);
             this.lbPassword.TabIndex = 2;
             this.lbPassword.Text = "Passwort";
             // 
@@ -78,9 +81,10 @@
             this.tfPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.tfPassword.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tfPassword.ForeColor = System.Drawing.Color.Black;
-            this.tfPassword.Location = new System.Drawing.Point(372, 231);
+            this.tfPassword.Location = new System.Drawing.Point(744, 444);
+            this.tfPassword.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.tfPassword.Name = "tfPassword";
-            this.tfPassword.Size = new System.Drawing.Size(267, 33);
+            this.tfPassword.Size = new System.Drawing.Size(532, 59);
             this.tfPassword.TabIndex = 3;
             this.tfPassword.UseSystemPasswordChar = true;
             // 
@@ -90,9 +94,10 @@
             this.btRegister.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btRegister.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btRegister.ForeColor = System.Drawing.Color.White;
-            this.btRegister.Location = new System.Drawing.Point(585, 308);
+            this.btRegister.Location = new System.Drawing.Point(1170, 592);
+            this.btRegister.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.btRegister.Name = "btRegister";
-            this.btRegister.Size = new System.Drawing.Size(56, 43);
+            this.btRegister.Size = new System.Drawing.Size(112, 83);
             this.btRegister.TabIndex = 5;
             this.btRegister.UseVisualStyleBackColor = false;
             this.btRegister.Click += new System.EventHandler(this.btRegister_Click);
@@ -103,10 +108,10 @@
             this.btLogin.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btLogin.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btLogin.ForeColor = System.Drawing.Color.White;
-            this.btLogin.Location = new System.Drawing.Point(372, 308);
-            this.btLogin.Margin = new System.Windows.Forms.Padding(2);
+            this.btLogin.Location = new System.Drawing.Point(744, 592);
+            this.btLogin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btLogin.Name = "btLogin";
-            this.btLogin.Size = new System.Drawing.Size(56, 43);
+            this.btLogin.Size = new System.Drawing.Size(112, 83);
             this.btLogin.TabIndex = 6;
             this.btLogin.UseVisualStyleBackColor = false;
             this.btLogin.Click += new System.EventHandler(this.btLogin_Click);
@@ -122,36 +127,41 @@
             this.paBackground.Controls.Add(this.tfUsername);
             this.paBackground.Controls.Add(this.lbPassword);
             this.paBackground.Controls.Add(this.tfPassword);
-            this.paBackground.Location = new System.Drawing.Point(-6, -1);
+            this.paBackground.Location = new System.Drawing.Point(-12, -2);
+            this.paBackground.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.paBackground.Name = "paBackground";
-            this.paBackground.Size = new System.Drawing.Size(682, 395);
+            this.paBackground.Size = new System.Drawing.Size(1364, 760);
             this.paBackground.TabIndex = 7;
             // 
             // paDes
             // 
+            this.paDes.AutoSize = true;
             this.paDes.BackgroundImage = global::CryptoKey.Properties.Resources.bild;
             this.paDes.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.paDes.Location = new System.Drawing.Point(4, 0);
+            this.paDes.Location = new System.Drawing.Point(8, 0);
+            this.paDes.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.paDes.Name = "paDes";
-            this.paDes.Size = new System.Drawing.Size(336, 406);
+            this.paDes.Size = new System.Drawing.Size(672, 781);
             this.paDes.TabIndex = 8;
             // 
             // label1
             // 
-            this.label1.Font = new System.Drawing.Font("Roboto Th", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
-            this.label1.Location = new System.Drawing.Point(445, 21);
+            this.label1.Location = new System.Drawing.Point(890, 40);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(124, 49);
+            this.label1.Size = new System.Drawing.Size(248, 94);
             this.label1.TabIndex = 7;
             this.label1.Text = "Log In";
             // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(669, 387);
+            this.ClientSize = new System.Drawing.Size(1338, 744);
             this.Controls.Add(this.paBackground);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "Login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
