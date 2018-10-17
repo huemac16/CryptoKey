9b723f27f78ed75e1051b8e26cb5aa920de78682
huemac16 <huemac16@htl-kaindorf.at>
2018-10-17 14:51:51 +0200

    lul

--- CryptoKey/CryptoKey/Login.Designer.cs
+++ CryptoKey/CryptoKey/Login.Designer.cs
@@ -157,6 +157,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(669, 387);
             this.Controls.Add(this.paBackground);
             this.Name = "Login";
